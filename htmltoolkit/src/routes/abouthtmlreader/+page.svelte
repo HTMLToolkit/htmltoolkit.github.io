58b152ed337945842bd8891b9073d0e2a408281f
NellowTCS <neeljaiswal23@gmail.com>
2025-10-01 10:41:13 -0600

    Add link to HTMLRunner in about HTML reader page

--- htmltoolkit/src/routes/abouthtmlreader/+page.svelte
+++ htmltoolkit/src/routes/abouthtmlreader/+page.svelte
@@ -5,4 +5,5 @@
 <section class="fade-in-down">
   <h2 class="text-2xl font-semibold mb-4">HTMLRunner</h2>
   <p>This is where the docs will go.</p>
+  <a class="text-lg mb-6" href="/HTMLRunner">Go to HTMLRunner</a>
 </section>
